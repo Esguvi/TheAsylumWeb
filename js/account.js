d8012ae5fd3453d1c1f9fc7d1039a92c38361e05
Esguvi <117128054+Esguvi@users.noreply.github.com>
2025-03-17 00:26:18 +0100

    Update account.js

--- js/account.js
+++ js/account.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.4.0/firebase
 import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyDT-A7mlLu6X3LRV5AFVm9xqzIRMBlWfkk",
+    authDomain: "theasylum-game.firebaseapp.com",
+    projectId: "theasylum-game",
+    storageBucket: "theasylum-game.firebasestorage.app",
+    messagingSenderId: "585770314222",
+    appId: "1:585770314222:web:33135f709bbca0969286ff",
+    measurementId: "G-XSDM25Q5D0"
 };
 
 const app = initializeApp(firebaseConfig);
